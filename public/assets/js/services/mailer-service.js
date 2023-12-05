b0aaa49b7ed099ff589e7351511b270beb9f1759
Maxime <139247550+Maxime-SALINS@users.noreply.github.com>
2023-12-05 16:45:49 +0100

    Update Formulaire

--- public/assets/js/services/mailer-service.js
+++ public/assets/js/services/mailer-service.js
@@ -6,7 +6,14 @@ console.log('assets/js/mailer-service.js chargé ✅');
  * objet contenant les informations soumises du formulaire {name : String, email : String, message : String}
  */
 function sendMail(infoContact) {
+	console.log(infoContact);
+	// TODO: Appel du service EmailJS pour envoyer un email
+	// avec les informations soumises du formulaire
+	// en paramètre
 
+	// Créer un compte sur https://www.emailjs.com/
+	// Créer des identifiants pour le service EmailJS SMTP
+	// Reporter les identifiants dans le code ci-dessous
 	// L'envoie d'email peut se vérifier dans l'interface d'EmailJS
 	return Email.send({
 		Host: "smtp.elasticemail.com",
@@ -15,7 +22,7 @@ function sendMail(infoContact) {
 		To: 'contact@gmail.com',
 		From: "themaxcraft13@gmail.com",
 		Subject: "Une demande de contact depuis le site Space Tour",
-		Body: "Une demande de contact a été envoyée depuis le site Space Tour. " + infoContact.message
+		Body: "Une demande de contact a été envoyée depuis le site Space Tour. " + infoContact.name + infoContact.email + infoContact.message
 	}).then(
 		// si l'email est envoyé, afficher une alerte de succès
 		message => alert(message, "Votre message a bien été envoyé !")
